14fafaf5cc105aebd5df7560eb99c712f6337510
Jobel Racines <jobelracines@gmail.com>
2021-02-13 07:17:35 +0000

    Fix lambda entry point
    Signed-off-by: Jobel Racines <jobelracines@gmail.com>

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app-module';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express';
+import * as express from 'express';
 import serverless from 'serverless-http';
 
 const server = express();
