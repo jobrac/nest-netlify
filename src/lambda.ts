a7bbe020737bccfe84fc010ccb9d5167f35abf1d
Jobel Racines <jobelracines@gmail.com>
2021-02-13 07:10:05 +0000

    Update
    Signed-off-by: Jobel Racines <jobelracines@gmail.com>

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app-module';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import express from 'express';
 import serverless from 'serverless-http';
