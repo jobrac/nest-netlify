d9c0c8c91f687549ea695c8d2b92045eaa85a256
Jobel Racines <jobelracines@gmail.com>
2021-02-13 06:52:10 +0000

    First commit

---
+++ src/lambda.ts
@@ -0,0 +1,30 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
+import serverless from 'serverless-http';
+
+const server = express();
+let cacheNest = false;
+
+const createNestServer = async (expressInstance) => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+  app.enableCors();
+  await app.init();
+};
+
+const handler = serverless(server);
+
+exports.handler = async (event: any, context: any) => {
+  if (!cacheNest) {
+    createNestServer(server)
+      .then(() => console.log('Nest Ready'))
+      .catch((err) => console.error('Nest broken', err));
+    cacheNest = true;
+  }
+
+  return await handler(event, context);
+};
