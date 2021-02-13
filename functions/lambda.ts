0f1682ddce83c62c66ae7bb42f5fdb7ab4fcd149
Jobel Racines <jobelracines@gmail.com>
2021-02-13 11:19:09 +0000

    Fix deploy zipped
    Signed-off-by: Jobel Racines <jobelracines@gmail.com>

--- functions/lambda.ts
+++ functions/lambda.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app-module';
+import { AppModule } from '../src/app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import * as express from 'express';
 import * as serverless from 'serverless-http';
