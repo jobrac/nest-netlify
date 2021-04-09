4f6eb1beffe4e3d3adb3f71935541ea4be9b77e4
Jobel Racines <jobelracines@gmail.com>
2021-04-09 22:34:05 +0800

    fix webpack library target

--- webpack-lambda.config.js
+++ webpack-lambda.config.js
@@ -2,8 +2,10 @@ module.exports = function (options, webpack) {
     return {
         ...options,
         entry: '/functions/lambda.ts',
+        target : "node",
         output:{
-            filename : 'lambda.js'
+            filename : 'lambda.js',
+            libraryTarget: 'umd'
         }
     }
 }
